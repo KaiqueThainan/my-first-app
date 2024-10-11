36987227eb2fec4216b636d3da036f9b1a1df644
kaique thainan <kaique.souza104@etec.sp.gov.br>
2024-10-10 22:14:55 -0300

    aula10.10

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Pressable, Text, TextInput, View } from "react-native"
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native"
 
 export function Form(){
     const [height, setHeight] = useState ('');
@@ -24,26 +24,92 @@ export function Form(){
     }
 
     return(
-        <View>
-         <View>
-            <Text>Altura:</Text>
+        <View style={styles.formContext}>
+         <View style={styles.form}>
+            <Text style={styles.formLabel}>Altura:</Text>
             <TextInput
              placeholder="Ex. 1.75"
-             value={height}    
+             value={height}
+             style={styles.formInput}    
              />
-             <Text>Peso:</Text>
+             <Text style={styles.formLabel}>Peso:</Text>
              <TextInput
              placeholder="Ex. 67.5"
              value={weight}
+             style={styles.formInput}  
             />
 
             <Pressable
             onPress={() => validatorImc()}
+            style={styles.formButton}
             >
-                <Text>Calcular</Text>
+                <Text style={styles.formButtonText}>Calcular</Text>
             </Pressable>
-            <Text>{imc}</Text>
+            <Text style={styles.formTextImc}>{imc}</Text>
          </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    formContext: {
+        width: "100%",
+        height:"100%" ,
+        bottom: 0,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        borderTopLeftRadius:30,
+        borderTopRightRadius:30,
+        marginTop: 30
+
+    }, 
+    form: {
+        width:"100%",
+        height: "auto",
+        marginTop:30,
+        padding:10,
+
+    },
+
+    formLabel: {
+
+    color:"#000000",
+    fontSize: 18,
+    paddingLeft:20
+    },
+
+    formInput: {
+        width:"90%",
+        height: 40,
+        borderRadius: 30,
+        backgroundColor: "#f6f6f6",
+        margin:12,
+        paddingLeft:10,
+},
+
+    formButton:{
+        borderRadius:50,
+        alignItems:"center",
+        justifyContent:"center",
+        width: "90%",
+        backgroundColor:"#ff0043",
+        paddingTop:14,
+        paddingBottom:14,
+        marginLeft:12,
+        margin: 30,
+    },
+    formButtonText: {
+        fontSize:20,
+        color:"#fff"
+    },
+
+    formTextImc:{
+        flex:1,
+        marginTop:15,
+        paddingTop:60,
+        borderRadius:50,
+        alignItems:"center",
+        width:"100%"
+    }
+
+})
